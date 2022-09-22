a1e373172950cb9d7c0988489a33fc025d71e59a
luckyuho <gsn915315@gmail.com>
2022-09-22 10:23:19 +0800

    Add files via upload

---
+++ day11/example_day11_test.go
@@ -0,0 +1,44 @@
+package bubble_sort
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+// 單元測試，表格驅動測試
+func TestBubbleSort(t *testing.T) {
+
+	input := make([]int, 10000)
+	for i := 0; i < 10000; i++ {
+		input[i] = rand.Intn(10000)
+	}
+
+	tests := []struct {
+		input    []int
+		expected []int
+	}{
+		// {[]int{4, 2, 7, 1, 3}, []int{1, 2, 3, 4, 7}},
+		// {[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}},
+
+		// 只為了展示，所以解答直接透過函數運算
+		{input, BubbleSort(input)},
+	}
+
+	for _, test := range tests {
+		result := BubbleSort(test.input)
+		equals(t, "BubbleSort()", "Given slice of integers", "Should return slice sorted in ascending order", result, test.expected)
+	}
+}
+
+func ExampleBubbleSort() {
+	result1 := BubbleSort([]int{4, 2, 7, 1, 3})
+	result2 := BubbleSort([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1})
+
+	fmt.Println(result1)
+	fmt.Println(result2)
+
+	// Output:
+	// [1 2 3 4 7]
+	// [1 2 3 4 5 6 7 8 9 10]
+}
